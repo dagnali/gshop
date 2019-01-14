1dfb0742490a48154235f78413d42c2f726bd1c6
daganli <782458127@qq.com>
2019-01-15 00:02:07 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <router-view></router-view>
+<FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+    </div>
+</template>
+
+<script>
+    import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+
+export default {
+  components: {FooterGuide},
+  comments:{
+      FooterGuide
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
